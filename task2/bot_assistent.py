106708427f1d3b73ef5dfb95a8efc21d266a440d
Lada <lada11lada@gmail.com>
2023-10-16 14:08:49 +0200

    implementations for adding, updating, and retreiving contacts

--- task2/bot_assistent.py
+++ task2/bot_assistent.py
@@ -1,4 +1,5 @@
 import json
+import os
 from constants import COMMANDS, CONTACTS_FILE
 from pathlib import Path
 
@@ -11,19 +12,39 @@ def main():
                 print("How can I help you?")
             elif command.startswith("add"):
                 try:
-                    _, name, phone = command.split()
+                    _, name, phone = command.split(' ')
                     add_contact(name, phone)
                     print("Contact added")
                 except ValueError:
                     print(f"Expecting command in form {COMMANDS.get('add')}")
-                    continue
             elif command.startswith("change"):
-                update_contact(command)
-                print("Contact updated.")
+                try:
+                    _, name, phone = command.split(' ')
+                    update_contact(name, phone)
+                    print("Contact updated")
+                except ValueError:
+                    print(
+                        f"Expecting command in form {COMMANDS.get('update')}")
+                except ValueNotFound:
+                    print(f"No contact for name {name} found")
             elif command.startswith("phone"):
-                show_contact(command)
+                try:
+                    _, name = command.split(' ')
+                    phone = show_contact(name)
+                    print(phone)
+                except ValueError:
+                    print(
+                        f"Expecting command in form {COMMANDS.get('phone')}")
+                except ValueNotFound:
+                    print(f"No contact for name {name} found")
             elif command.startswith("all"):
-                show_all_contacts()
+                try:
+                    contacts = get_contacts()
+                    for contact in contacts:
+                        print(f"{contact.get('name'):<10}{contact.get('phone'):<10}")
+                except FileNotFoundError:
+                    # if the file is empty there are no contacts to show
+                    print("We haven't stored any contacts yet")
             elif command in ["close", "exit"]:
                 print("Good bye!")
                 break
@@ -34,30 +55,66 @@ def main():
                    .format('\n'.join(['- ' + s for s in COMMANDS.values()]))))
             continue
 
-
+# exception when invalid commdand was entered by the user
 class InvalidCommandException(Exception):
     pass
 
+# exception raised when value wasn't found
+class ValueNotFound(Exception):
+    pass
+
+file_path = Path.cwd().joinpath(CONTACTS_FILE)
 
 def add_contact(name: str, phone: str):
-    file_path = Path.cwd().joinpath(CONTACTS_FILE)
-    contact = {
-        "name": name,
-        "phone": phone
-    }
-    with open(file_path, 'w+') as file:
-        json.dump(contact, file)
+    try:
+        data_list = get_contacts()
+    except FileNotFoundError:
+        # if there is no file yet or it is empty, we just create one and add contact
+        data_list = []
 
-def update_contact(command):
-    pass
+    contact = {"name": name, "phone": phone}
+    data_list.append(contact)
 
-
-def show_all_contacts(command):
-    pass
+    with open(file_path, 'w') as json_file:
+        json.dump(data_list, json_file, indent=4)
 
 
-def show_contact(command):
-    pass
+def update_contact(name, phone):
+    try:
+        contacts = get_contacts()
+        updated = False
+        for c in contacts:
+            if c.get("name") == name:
+                c["phone"] = phone
+                updated = True
+        with open(file_path, 'w') as json_file:
+            json.dump(contacts, json_file, indent=4)
+        if not updated:
+            raise ValueNotFound
+    except FileNotFoundError:
+        # if the file is empty, our name isn't in the file
+        raise ValueNotFound
+
+def show_contact(name):
+    try:
+        contacts = get_contacts()
+        for c in contacts:
+            if c.get("name") == name:
+                return c["phone"]
+        # if there was no match, then there is no such contact stored
+        raise ValueNotFound
+    except FileNotFoundError:
+        # if the file is empty, our name isn't in the file
+        raise ValueNotFound
+
+def get_contacts():
+    file_not_empty = os.path.isfile(file_path) and os.path.getsize(file_path) > 0
+    if file_not_empty:
+        with open(file_path, 'r') as json_file:
+            data_list = json.load(json_file)
+            return data_list
+    else:
+        raise FileNotFoundError
 
 
 if __name__ == "__main__":
